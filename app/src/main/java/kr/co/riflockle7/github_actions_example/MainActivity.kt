2cbe00733d67bc51adad9cfce0a039e304be8f49
Sang Woo Lee <ricky@rickyui-MacBookPro.local>
2021-11-21 22:45:12 +0900

    first commit

---
+++ app/src/main/java/kr/co/riflockle7/github_actions_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.riflockle7.github_actions_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
